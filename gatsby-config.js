bdf4d2ac3219521e2563ae78d9a30bed89d7ef13
Mathieu Gauthier <postmaster@mathieugauthier.fr>
2020-02-08 13:32:16 +0100

    update csp

--- gatsby-config.js
+++ gatsby-config.js
@@ -191,7 +191,7 @@ module.exports = {
             `Content-Security-Policy: block-all-mixed-content; base-uri 'self'; default-src 'self' data: i.ytimg.com ${cloudfrontId}.cloudfront.net; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; object-src 'none'; form-action 'self'; font-src 'self' data: ${cloudfrontId}.cloudfront.net fonts.gstatic.com; connect-src 'self' www.googleapis.com`,
           ],
           '/admin/*': [
-            `Content-Security-Policy: block-all-mixed-content; base-uri 'self'; default-src 'self' data: raw.githubusercontent.com; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; object-src 'none'; form-action 'self'; font-src 'self' data:; connect-src 'self'`,
+            `Content-Security-Policy: block-all-mixed-content; base-uri 'self'; default-src 'self' blob: data: raw.githubusercontent.com; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; object-src 'none'; form-action 'self'; font-src 'self' data:; connect-src 'self'`,
           ],
           '/icons/*.png': [`cache-control: public, max-age=31536000,immutable`],
         },
